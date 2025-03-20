a905d0dff08221ad9116d10ce85259b4526cc79e
barry <barry.huang@dudooeat.com>
2025-03-20 16:06:22 +0800

    排除dd3ServerGraphQL folder

--- src/requests/makeComplexRequest.ts
+++ src/requests/makeComplexRequest.ts
@@ -11,9 +11,10 @@ export const makeComplexRequest = async (
   }
 ) => {
   const excludedPatterns = [
-    /\/generated\.go$/,      // 精確匹配 "generated.go"
-    /\/models_gen\.go$/,     // 精確匹配 "models_gen.go"
-    /.*_settings\.json$/,    // 匹配任意前綴 + "settings.json"
+    /\/generated\.go$/,       // 精確匹配 "generated.go"
+    /\/models_gen\.go$/,      // 精確匹配 "models_gen.go"
+    /.*_settings\.json$/,     // 匹配任意前綴 + "settings.json"
+    /\/dd3ServerGraphQL\/.*/, // 匹配 dd3ServerGraphQL
   ];
   const pullRequests = await getPullRequests(amount, repository);
 
